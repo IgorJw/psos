ad204fe4271cba1cb2bb0f0dc815dc7a28eb035f
kmaliszewski <kuba.maliszewski01@gmail.com>
2023-03-22 21:10:05 +0100

    Endpoints corrected

--- src/main/java/com/example/psostest/User/Service/UsersService.java
+++ src/main/java/com/example/psostest/User/Service/UsersService.java
@@ -2,18 +2,17 @@ package com.example.psostest.User.Service;
 
 import com.example.psostest.User.Entity.UsersBasicInfo;
 import com.example.psostest.User.Repository.UsersBasicInfoRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
 
 @Service
+@AllArgsConstructor
 public class UsersService {
 
     private UsersBasicInfoRepository usersBasicInfoRepository;
 
-    public UsersService(UsersBasicInfoRepository usersBasicInfoRepository) {
-        this.usersBasicInfoRepository = usersBasicInfoRepository;
-    }
 
     public UsersBasicInfo getUserBasicInfoByUserId(Integer userId) {
         UsersBasicInfo userInfo = usersBasicInfoRepository.findByUserId(userId);
