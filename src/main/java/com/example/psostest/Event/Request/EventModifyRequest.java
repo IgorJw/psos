7670620927c7ca75626f8611060159528e21dcab
kmaliszewski <kuba.maliszewski01@gmail.com>
2023-04-09 13:25:48 +0200

    basic put endpoints for event and subject

---
+++ src/main/java/com/example/psostest/Event/Request/EventModifyRequest.java
@@ -0,0 +1,19 @@
+package com.example.psostest.Event.Request;
+
+import com.example.psostest.Event.Enum.EventPriority;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventModifyRequest {
+    Integer eventId;
+    String content;
+    LocalDate date;
+    EventPriority priority;
+    Integer subjectId;
+}
---
+++ src/main/java/com/example/psostest/Subject/Request/SubjectDeleteRequest.java
@@ -0,0 +1,12 @@
+package com.example.psostest.Subject.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubjectDeleteRequest {
+    Integer subjectId;
+}
